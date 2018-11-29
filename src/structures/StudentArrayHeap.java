8c8506f0c949008ccfc46fb1228000f270f09b50
Daniel Seriy <DanielSeriy00@gmail.com>
2018-11-28 20:15:12 -0500

    second commit

--- src/structures/StudentArrayHeap.java
+++ src/structures/StudentArrayHeap.java
@@ -4,5 +4,52 @@ import java.util.Comparator;
 import java.util.Iterator;
 
 public class StudentArrayHeap<P, V> extends AbstractArrayHeap<P, V> {
-}
 
+	protected StudentArrayHeap(Comparator<P> comparator) {
+		super(comparator);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected int getLeftChildOf(int index) throws IndexOutOfBoundsException {
+		// TODO Auto-generated method stub
+		if(index < 0) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		return (2 * index) + 1;
+	}
+
+	@Override
+	protected int getRightChildOf(int index) {
+		// TODO Auto-generated method stub
+		if(index < 0) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		return (2 * index) + 2;
+	}
+
+	@Override
+	protected int getParentOf(int index) {
+		// TODO Auto-generated method stub
+		if(index < 1) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		return (index - 1) / 2;
+	}
+
+	@Override
+	protected void bubbleUp(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void bubbleDown(int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
